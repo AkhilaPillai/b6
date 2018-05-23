40309ec017f7e71d26b16e72168d1c823219f0aa
AkhilaPillai <36732292+AkhilaPillai@users.noreply.github.com>
2018-05-23 12:29:16 +0530

    Create sort.cpp

---
+++ sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a[100],n,i,j,b;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+        
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+           if(a[i]>a[j])
+           {
+               b=a[i];
+               a[i]=a[j];
+               a[j]=b;
+               
+           }
+        }
+    }
+    for(i=0;i<n;i++)
+    cout<<a[i];
+    return 0;
+}
